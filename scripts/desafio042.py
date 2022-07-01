1cebad63a280a2169ffc8b22b43ad2a86544ddf3
User7558 <82788197+User7558@users.noreply.github.com>
2022-07-01 16:39:46 -0300

    .
    finalmente consigo postar dnv

---
+++ scripts/desafio042.py
@@ -0,0 +1,7 @@
+#desafio042: faça um programa que leia um número entre 1 e 10 e imprima o número de vezes que o valor representa
+#
+n = int(input('Digite um número: '))
+c = 0
+while c < n:
+  print(f'{n} \n')
+  c += 1
