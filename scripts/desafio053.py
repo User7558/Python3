8e6f910bf3d2d60f75e777714d8fa501e1ab09bd
User7558 <82788197+User7558@users.noreply.github.com>
2022-08-21 19:40:24 -0300

    arrumei endentação

--- scripts/desafio053.py
+++ scripts/desafio053.py
@@ -8,12 +8,12 @@ lado1 = float(input('Informe o tamanho do primeiro lado: '))
 lado2 = float(input('Informe o tamanho do segundo lado: '))
 
 if base < lado1 + lado2 and lado1 < base + lado2 and lado2 < base + lado1:
-    print('O triângulo É possivel neste caso.')
-    if base==lado1==lado2:
-      print('O triângulo será equilátero.')
-    elif base==lado1 or base==lado2 or lado1==lado2:
-      print('O triângulo será isósceles.')
-    else:
-      print('O triângulo será escaleno.')
+  print('O triângulo É possivel neste caso.')
+  if base==lado1==lado2:
+    print('O triângulo será equilátero.')
+  elif base==lado1 or base==lado2 or lado1==lado2:
+    print('O triângulo será isósceles.')
+  else:
+    print('O triângulo será escaleno.')
 else:
-    print('O triângulo NÃO é possível neste caso.')
+  print('O triângulo NÃO é possível neste caso.')
