f8f74500a43fc9d7a08fd10074277b31ce6ca185
User7558 <82788197+User7558@users.noreply.github.com>
2022-08-21 19:03:56 -0300

    .

--- scripts/desafio052.py
+++ scripts/desafio052.py
@@ -1,6 +1,6 @@
 '''Desafio052: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
--se ele ainda vai se alistar ao serviço militar (-17)
--se já é a hora de se alistar (17/18)
+-se ele ainda vai se alistar ao serviço militar (-18)
+-se já é a hora de se alistar (18)
 -se já passou do tempo de alistamento (+18)
 Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.'''
 #
@@ -10,10 +10,11 @@ an = int(input('Em qual ano você nasceu: '))
 at = date.today().year
 i = at - an 
 
-if i < 17:
-  f = 17 - i 
+print(f'Atualmente você possui {i} anos...')
+if i < 18:
+  f = 18 - i 
   print(f'Você deverá se alistar no exército daqui {f} anos.')
-elif i >= 17 and i <= 18:
+elif i == 18:
   print('É sua hora de se alistar no exército')
 else:
   f = i - 18 
