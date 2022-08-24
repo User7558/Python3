6e37f05b99eaba025981b367d2102ae72e1fe480
Jack <82788197+User7558@users.noreply.github.com>
2022-08-23 22:08:59 -0300

    adicionei um novo desafio

---
+++ scripts/desafio055.py
@@ -0,0 +1,34 @@
+'''Desafio055: Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e a condição de pagamento:
+- à vista dinehiro/cheque: 10% de desconto;
+- à vista no cartão: 5% de desconto;
+- em até 2x no cartão: preço normal;
+- 3x ou mais vezes no cartão: 20% de juros.'''
+#
+x = float(input('Qual o preço do produto que deseja comprar? R$'))
+fp = int(input('''Qual a forma de pagamento?
+[1] Dinheiro ou cheque (oferece 10% de desconto)
+[2] Cartão
+------------------------------------------------
+R: '''))
+
+if fp==1:
+    d = (x*10)/100
+    x = x-d
+    print(f'O desconto oferecido graças a forma de pagamento de seu produto foi de 10%, o valor passará a ser R${x:.2f}.')
+elif fp==2:
+    p = int(input('''Em quantas parcelas pagará seu produto?
+[1] À vista
+[2] 2x
+[3] 3x ou mais
+---------------
+R: '''))
+    if p==1:
+        d = (x*5)/100
+        x = x-d
+        print(f'O desconto oferecido graças a forma de pagamento de seu produto foi de 5%, o valor passará a ser R${x:.2f}.')
+    elif p==2:
+        print(f'A forma de pagamento escolhida não dá direito a desconto, o valor a ser pago será de R${x:.2f}.')
+    elif p==3:
+        d = (x*20)/100
+        x += d
+        print(f'O juros agregado graças a forma de pagamento de seu produto foi de 20%, o valor passará a ser R${x:.2f}.')
