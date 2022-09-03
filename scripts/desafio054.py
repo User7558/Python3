346bf87861863e97111958495ab9fbd51dd17c02
User7558 <82788197+User7558@users.noreply.github.com>
2022-09-03 16:40:46 -0300

    Update desafio054.py

--- scripts/desafio054.py
+++ scripts/desafio054.py
@@ -7,15 +7,15 @@
 #
 peso = float(input('Qual seu peso (kg)? '))
 alt = float(input('Qual sua altura (cm)? '))
-imc = peso/(alt)**2
+imc = peso/(alt**2)
 
 if imc<18.5:
-  print(f'Seu IMC é {imc:.2f}, você está \033[1;34mabaixo\033[m do peso ideal.')
-elif imc>=18.5 and imc<=25:
-  print(f'Seu IMC é {imc:.2f}, você está no \033[1;32mpeso ideal\033[m.')
-elif imc>=25 and imc<=30:
-  print(f'Seu IMC é {imc:.2f}, você está em estado de \033[1;35msobrepeso\033[m.')
-elif imc>=30 and imc<=40:
-  print(f'Seu IMC é {imc:.2f}, você está em estado de \033[1;33mobesidade\033[m.')
+  print(f'Seu IMC é {imc:.1f}, você está \033[1;34mabaixo\033[m do peso ideal.')
+elif 18.5<=imc<25:
+  print(f'Seu IMC é {imc:.1f}, você está no \033[1;32mpeso ideal\033[m.')
+elif 25<=imc<30:
+  print(f'Seu IMC é {imc:.1f}, você está em estado de \033[1;35msobrepeso\033[m.')
+elif 30<=imc<40:
+  print(f'Seu IMC é {imc:.1f}, você está em estado de \033[1;33mobesidade\033[m.')
 else:
-  print(f'Seu IMC é {imc:.2f}, você está em estado de \033[1;31mobesidade mórbida\033[m.')
+  print(f'Seu IMC é {imc:.1f}, você está em estado de \033[1;31mobesidade mórbida\033[m.')
