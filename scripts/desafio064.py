b934c9774fe30483b1522aabbb028a750c3b0a84
User7558 <82788197+User7558@users.noreply.github.com>
2022-10-15 15:52:33 -0300

    Update desafio064.py

--- scripts/desafio064.py
+++ scripts/desafio064.py
@@ -1,7 +1,27 @@
 #Desafio064: Crie um programa que leia uma frase qualquer e diga se ela é um PALÍNDROMO, desconsiderando os espaços.
 #
-frase = str(input('Insira uma frase para verificarmos se ela é um PALÍNDROMO: '))
+frase = str(input('Insira uma frase para verificarmos se ela é um PALÍNDROMO: ')).strip().lower()
 junto = frase.replace(' ','')
-#???
-print(frase)
-print(junto)
+inverso = ''
+
+for letra in range (len(junto)-1, -1, -1):
+  inverso += junto[letra]
+
+if inverso==junto:
+  print(f'A frase {frase} é um palíndromo!')
+else:
+  print(f'A frase {frase} não é um palíndromo...')
+print(f'Frase digitada: {junto}. \nInverso: {inverso}.')
+
+#MÉTODO DE FATIAMENTO
+'''
+frase = str(input('Insira uma frase para verificarmos se ela é um PALÍNDROMO: ')).strip().lower()
+junto = frase.replace(' ','')
+inverso = junto[::-1]
+
+if inverso==junto:
+  print(f'A frase {frase} é um palíndromo!')
+else:
+  print(f'A frase {frase} não é um palíndromo...')
+print(f'Frase digitada: {junto}. \nInverso: {inverso}.')
+'''
