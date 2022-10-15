29829e62214c7187f70f06b391f06df1dcbc4191
User7558 <82788197+User7558@users.noreply.github.com>
2022-10-15 16:06:47 -0300

    .

--- scripts/desafio065.py
+++ scripts/desafio065.py
@@ -7,12 +7,13 @@ maiores = 0
 menores = 0
 
 for c in range (0, 7):
-  nasc = int(input('Insira seu ano de nascimento: '))
+  nasc = int(input(f'Insira seu ano de nascimento da {c+1}ª pessoa: '))
   idade = date.today().year - nasc
   if idade >= 21:
     maiores += 1
   else:
     menores += 1
 
+print(' ')
 print(f'Total de pessoas que são maoires de idade (+21 anos): {maiores}.')
 print(f'Total de pessoas que não atingiram a maioridade (-21 anos): {menores}.')
--- scripts/desafio066.py
+++ scripts/desafio066.py
@@ -1,14 +1,18 @@
 #Desafio066: Faça um programa que leia o peso de 5 pessoas. no final, mostre qual foi o maior e o menor peso lidos.
 #
-menor = 99999
+menor = 0
 maior = 0
 
 for c in range (0,5):
-  peso = float(input('Insira seu peso (em kg): '))
-  if peso > maior:
+  peso = float(input(f'Insira o peso (em kg) da {c+1}ª pessoa: '))
+  if c==1:
     maior = peso
-  elif peso < menor:
     menor = peso
+  else:
+    if peso > maior:
+      maior = peso
+    elif peso < menor:
+      menor = peso
 
 print(f'Menor peso registrado: {menor}kg.')
 print(f'Maior peso registrado: {maior}kg.')
